63bc2318d6790e8ec9fcba1105e296511efb005b
buddywinangun <buddywinangun@gmail.com>
2023-10-20 10:36:35 +0700

    added: comment

--- tasks/sync.js
+++ tasks/sync.js
@@ -17,17 +17,32 @@ const config = require('../config');
 // -- GULP TASKS
 // ---------------------------------------------------
 
-gulp.task('runServer', () => {
-  return browserSync.init({
+gulp.task('runServer', done => {
+
+	// Make sure this feature is activated before running
+	if (!config.settings.reload) return done();
+
+	// Initialize BrowserSync
+  browserSync.init({
     server: {
       baseDir: [config.paths.build(config.project)]
     },
     port: 8080,
     open: true
   });
+
+	// Signal completion
+	done();
 });
 
 gulp.task('reload', done => {
+
+	// Make sure this feature is activated before running
+	if (!config.settings.reload) return done();
+
+  // Reload the browser when files change
   browserSync.reload();
+
+	// Signal completion
   done();
 });
