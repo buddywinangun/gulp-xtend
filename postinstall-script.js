7fa235271c60c12eb54f120ebed9e3b1846a8428
buddywinangun <buddywinangun@gmail.com>
2023-11-08 20:58:42 +0700

    changed: postinstall-script

--- postinstall-script.js
+++ postinstall-script.js
@@ -8,11 +8,13 @@
 'use strict';
 
 const fs = require("fs");
+const path = require("path");
 
 // User's local directory
 const userPath = process.env.INIT_CWD
 
-if(__dirname.includes('node_modules')) {
+if(__dirname.includes('node_modules')
+  && fs.existsSync(path.join(userPath, 'config.js')) === false) {
   // Moving files to user's local directory
   fs.cpSync('start', userPath, {
     recursive: true
